b12fe4250104d24252aa7e9d011fa271a79fb8da
Soonyoung Park <soon.syp@gmail.com>
2023-02-25 15:30:59 +0100

    trigger raycaster when touchstart

--- src/Application/Application.js
+++ src/Application/Application.js
@@ -45,6 +45,9 @@ export default class Application {
         this.mouse.on('mousemove', () => {
             this.raycaster.update()
             this.world.intersect()
+            if (this.isTouchDevice) {
+                this.world.mousedown()
+            }
         })
 
         this.mouse.on('scroll', () => {
@@ -53,9 +56,6 @@ export default class Application {
 
         this.mouse.on('click', () => {
             this.world.click()
-            if (this.isTouchDevice) {
-                this.world.mousedown()
-            }
         })
     }
 
