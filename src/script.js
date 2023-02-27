a86250c5d33286e3c8345b0d9c44b9e3083be52d
Soonyoung Park <soon.syp@gmail.com>
2023-02-27 16:58:53 +0100

    update contents

--- src/script.js
+++ src/script.js
@@ -4,15 +4,20 @@ import Application from './Application/Application.js'
 const application = new Application(document.querySelector('canvas.webgl'))
 
 // Get the modal
-const robotAppModal = document.getElementById("robotAppModal")
-const robotApp = document.getElementById("robotApp")
+const autoModeModal = document.getElementById("autoModeModal")
+const autoMode = document.getElementById("autoMode")
+const scheduleCleaningModal = document.getElementById("scheduleCleaningModal")
+const scheduleCleaning = document.getElementById("scheduleCleaning")
 const villageModal = document.getElementById("villageModal")
 const village = document.getElementById("village")
 const multicultureModal = document.getElementById("multicultureModal")
 const multiculture = document.getElementById("multiculture")
 
-robotApp.onclick = function () {
-    robotAppModal.style.display = "block"
+autoMode.onclick = function () {
+    autoModeModal.style.display = "block"
+}
+scheduleCleaning.onclick = function () {
+    scheduleCleaningModal.style.display = "block"
 }
 village.onclick = function () {
     villageModal.style.display = "block"
@@ -21,26 +26,33 @@ multiculture.onclick = function () {
     multicultureModal.style.display = "block"
 }
 
-const closeRobot = document.getElementsByClassName("close")[0]
-closeRobot.onclick = function () {
-    robotAppModal.style.display = "none"
+const closeAutoMode = document.getElementsByClassName("close")[0]
+closeAutoMode.onclick = function () {
+    autoModeModal.style.display = "none"
 }
 
-const closeVillage = document.getElementsByClassName("close")[1]
+const cloaseSchedule = document.getElementsByClassName("close")[1]
+cloaseSchedule.onclick = function () {
+    scheduleCleaningModal.style.display = "none"
+}
+
+const closeVillage = document.getElementsByClassName("close")[2]
 closeVillage.onclick = function () {
     villageModal.style.display = "none"
 }
 
-const closeMulticulture = document.getElementsByClassName("close")[2]
+const closeMulticulture = document.getElementsByClassName("close")[3]
 closeMulticulture.onclick = function () {
     multicultureModal.style.display = "none"
 }
 
 window.onclick = function (event) {
-    if (event.target == robotAppModal ||
+    if (event.target == autoModeModal ||
+        event.target == scheduleCleaningModal ||
         event.target == villageModal ||
         event.target == multicultureModal) {
-        robotAppModal.style.display = "none"
+        autoModeModal.style.display = "none"
+        scheduleCleaningModal.style.display = "none"
         villageModal.style.display = "none"
         multicultureModal.style.display = "none"
     }
