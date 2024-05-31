d30569c1949575e9bbfeac8a65497b5e4eea4f3d
Soonyoung Park <soon.syp@gmail.com>
2024-05-31 11:52:33 +0200

    update

--- src/Application/Camera.js
+++ src/Application/Camera.js
@@ -56,11 +56,11 @@ export default class Camera {
         this.xTilt = Math.round(event.gamma) * 0.3; // y axis
         break;
       case "portrait-secondary":
-        this.yTilt = Math.round(event.beta+90) * 0.3;  // x axis
-        this.xTilt = Math.round(event.gamma) * 0.3; // y axis
+        // this.yTilt = Math.round(event.beta-90) * 0.3;  // x axis
+        // this.xTilt = Math.round(event.gamma) * 0.3; // y axis
       case "landscape-primary":
-        this.yTilt = Math.round(event.beta) * 0.3;  // x axis
-
+        this.yTilt = Math.round(event.beta-90) * 0.3;  // x axis
+        this.xTilt = Math.round(event.gamma) * 0.3; // y axis
         break;
       case "landscape-secondary":
         break;
