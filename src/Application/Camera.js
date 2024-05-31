7078bfd3f427b704f2b328273f18d874475119c9
Soonyoung Park <soon.syp@gmail.com>
2024-05-31 14:12:11 +0200

    update

--- src/Application/Camera.js
+++ src/Application/Camera.js
@@ -58,11 +58,11 @@ export default class Camera {
         break;
       case "landscape-primary":
         this.yTilt = event.gamma > 0 ? (event.gamma-90) * 0.2 :(event.gamma+90) * 0.2; 
-        this.xTilt = event.beta * 0.2;  
+        this.xTilt = event.gamma > 0 ? (event.gamma-180) * 0.2 :(event.gamma+180) * 0.2;  
         break;
       case "landscape-secondary":
         this.yTilt = event.gamma > 0 ? (event.gamma-90) * 0.2 :(event.gamma+90) * 0.2;  
-        this.xTilt = event.beta * 0.2;  
+        this.xTilt = event.gamma > 0 ? (event.gamma-180) * 0.2 :(event.gamma+180) * 0.2;  
         break;
       default:
         permission.style.display = "none";
