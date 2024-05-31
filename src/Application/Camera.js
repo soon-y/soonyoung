7cd6adb9c2ce96a4d6e336db7cb5353210710802
Soonyoung Park <soon.syp@gmail.com>
2024-05-31 12:08:26 +0200

    update

--- src/Application/Camera.js
+++ src/Application/Camera.js
@@ -53,14 +53,14 @@ export default class Camera {
     switch (screen.orientation.type) {
       case "portrait-primary":
         this.yTilt = Math.round(event.beta-90) * 0.3;  // x axis
-        this.xTilt = -Math.round(event.gamma) * 0.3; // y axis
+        this.xTilt = Math.round(event.gamma) * 0.3; // y axis
         break;
       case "portrait-secondary":
         // this.yTilt = Math.round(event.beta-90) * 0.3;  // x axis
         // this.xTilt = Math.round(event.gamma) * 0.3; // y axis
       case "landscape-primary":
         this.xTilt = Math.round(event.beta) * 0.3;  // x axis
-        this.yTilt = -Math.round(event.gamma) * 0.3; // y axis
+        // this.yTilt = -Math.round(event.gamma) * 0.3; // y axis
         break;
       case "landscape-secondary":
         break;
